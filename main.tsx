5bccca35c3226078306ba59fe795a3fb9d8e07dd
GitRamaKrishna2187 <rk.ramakrishna07@gmail.com>
2026-05-09 21:30:18 +0530

    Refactor imports in main.tsx for EPass

--- main.tsx
+++ main.tsx
@@ -7,7 +7,8 @@ import './index.css'
 import Home from './pages/Home'
 import Buses from './pages/Buses'
 import BusDetail from './pages/BusDetail'
-import { EPass, Timetable, Profile } from './pages/Placeholders'
+import EPass from './pages/EPass'
+import { Timetable, Profile } from './pages/Placeholders'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
