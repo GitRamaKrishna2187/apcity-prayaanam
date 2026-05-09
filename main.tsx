79a22865802333a688a1475534d607290cd39879
GitRamaKrishna2187 <rk.ramakrishna07@gmail.com>
2026-05-09 15:41:23 +0530

    Refactor route imports and structure in main.tsx

--- main.tsx
+++ main.tsx
@@ -4,25 +4,21 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { LanguageProvider } from './i18n/LanguageContext'
 import './index.css'
 
-import Home    from './pages/Home'
-import Buses   from './pages/Buses'
-import BusDetail from './pages/BusDetail'
-import EPass   from './pages/EPass'
-import Timetable from './pages/Timetable'
-import Profile from './pages/Profile'
+import Home from './pages/Home'
+import { Buses, BusDetail, EPass, Timetable, Profile } from './pages/Placeholders'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LanguageProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/"            element={<Home />} />
-          <Route path="/buses"       element={<Buses />} />
-          <Route path="/bus/:id"     element={<BusDetail />} />
-          <Route path="/epass"       element={<EPass />} />
-          <Route path="/timetable"   element={<Timetable />} />
-          <Route path="/profile"     element={<Profile />} />
-          <Route path="*"            element={<Navigate to="/" replace />} />
+          <Route path="/"          element={<Home />} />
+          <Route path="/buses"     element={<Buses />} />
+          <Route path="/bus/:id"   element={<BusDetail />} />
+          <Route path="/epass"     element={<EPass />} />
+          <Route path="/timetable" element={<Timetable />} />
+          <Route path="/profile"   element={<Profile />} />
+          <Route path="*"          element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </LanguageProvider>
